11e9708b9d90683b3b1c9564ee74b30d02dd83ce
Daniel Rodriguez <dsrodriguezmes@uninpahu.edu.co>
2025-10-03 07:56:15 -0500

    pilas

---
+++ Pila.php
@@ -0,0 +1,19 @@
+<?php
+
+$autolavado =  array();
+array_push($autolavado, "auto 1");
+array_push($autolavado, "auto 2");
+array_push($autolavado, "auto 3");
+array_push($autolavado, "auto 4");
+array_push($autolavado, "auto 5");
+array_push($autolavado, "auto 6");
+array_push($autolavado, "auto 7");
+array_push($autolavado, "auto 8");
+array_push($autolavado, "auto 9");
+
+$auto = array_pop($autolavado);
+
+echo "Cual es el primer auto en salir".$auto;
+
+
+?>
